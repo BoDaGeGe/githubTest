bd88ddea23f1c58a74e6c2578328b0f4d42bd9d0
BoDaGeGe <877126670@qq.com>
2022-04-10 00:46:42 +0800

    测试，第五次提交

--- githubTest/githubTest/main.cpp
+++ githubTest/githubTest/main.cpp
@@ -1,11 +1,44 @@
 #include <iostream>
 #include <Windows.h>
+#include <string>
+
+/*
+1¡¢ÍøÕ¾404¹¥»÷
+2¡¢ÍøÕ¾´Û¸Ä¹¥»÷
+3¡¢ÍøÕ¾¹¥»÷¼ÇÂ¼
+4¡¢DNS¹¥»÷
+5¡¢·þÎñÆ÷ÖØÆô¹¥»÷
+*/
+
 
 int main(void)
 {
-	int a;
-	std::cin >> a;
-	std::cout << "hi!" << std::endl;
+	std::string name;
+	std::string pwd;
+
+	while (1)
+	{
+		std::cout << "ÇëÊäÈëÕËºÅ£º";
+		std::cin >> name;
+
+		std::cout << "ÇëÊäÈëÃÜÂë£º";
+		std::cin >> pwd;
+
+		if (name == "54hk" && pwd == "123456")
+		{
+			break;
+		}
+		else
+		{
+			std::cout << "ÓÃ»§Ãû»òÃÜÂë´íÎó" << std::endl;
+		}
+	}
+
+	std::cout << "1¡¢ÍøÕ¾404¹¥»÷" << std::endl;
+	std::cout << "2¡¢ÍøÕ¾´Û¸Ä¹¥»÷" << std::endl;
+	std::cout << "3¡¢ÍøÕ¾¹¥»÷¼ÇÂ¼" << std::endl;
+	std::cout << "4¡¢DNS¹¥»÷" << std::endl;
+	std::cout << "5¡¢·þÎñÆ÷ÖØÆô¹¥»÷" << std::endl;
 
 	system("pause");
 	return 0;
